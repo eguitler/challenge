92297d254351aab29ef1acf2f722e1971b5de18b
Ezequiel Guitler <eguitler@gmail.com>
2022-04-01 23:13:42 -0300

    [fix] HeroImage style

--- src/components/HeroImage/styles.js
+++ src/components/HeroImage/styles.js
@@ -4,24 +4,31 @@ export const Container = styled.div`
   width: fit-content;
   position: absolute;
   right: 0;
+  top: 0;
   z-index: -1;
-  height: 100%;
+  width: 100%;
+  max-width: 60rem;
 `;
 
 export const Overlay = styled.div`
   --bg: ${({ theme }) => theme.colors.contrast};
-  --gradient: 90deg, var(--bg) 4%, rgba(255,255,255,0) 50%;
+  --left_gradient: 90deg, var(--bg) 2%, rgba(255,255,255,0) 50%;
+  --right_gradient: 0deg, var(--bg) 4%, rgba(255,255,255,0) 25%;
 
   position: absolute;
   width: 100%;
   height: 100%;
   background: var(--bg);
-  background: -moz-linear-gradient(var(--gradient));
-  background: -webkit-linear-gradient(var(--gradient));
-  background: linear-gradient(var(--gradient));
+  background: -moz-linear-gradient(var(--left_gradient)),
+              -moz-linear-gradient(var(--right_gradient));
+
+  background: -webkit-linear-gradient(var(--left_gradient)),
+              -webkit-linear-gradient(var(--right_gradient));
+
+  background: linear-gradient(var(--left_gradient)),
+              linear-gradient(var(--right_gradient));
 `;
 
 export const Image = styled.img`
-  max-height: 100%;
   width: 100%;
 `;
