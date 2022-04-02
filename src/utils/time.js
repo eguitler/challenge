35848ba0e459ef278c9e63e9865faf2752e3e231
Ezequiel Guitler <eguitler@gmail.com>
2022-04-01 23:19:21 -0300

    [feat] helper durationMinToHours implemented

---
+++ src/utils/time.js
@@ -0,0 +1,9 @@
+export const durationMinToHours = (min) => {
+  const hours = Math.floor(min / 60);
+  const minutes = min - (60 * hours);
+
+  if (hours === 0) return `${min} min`;
+  if (minutes === 0) return `${hours} hr`;
+
+  return `${hours} hs ${minutes} min`;
+};
