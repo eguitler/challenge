e091b87bc6ff4697871885cc9bc58c483b203222
Ezequiel Guitler <eguitler@gmail.com>
2022-04-03 23:21:21 -0300

    [feat] added hover to cards

--- src/components/TrailerCard/styles.js
+++ src/components/TrailerCard/styles.js
@@ -1,29 +1,49 @@
 import styled from 'styled-components';
+import { PLAY } from '../../assets';
 
 export const Container = styled.article`
   width: 100%;
+  height: min-content;
   position: relative;
-  margin-bottom: 2rem;
   cursor: pointer;
+  margin-bottom: 2rem;
+  display: grid;
 `;
 
 export const Img = styled.img`
   width: 100%;
 `;
 
-export const Overlay = styled.article`
+export const Overlay = styled.div`
   --bg: ${({ theme }) => theme.colors.contrast};
-  --bottom_gradient: 0deg, var(--bg) 0%, rgba(255,255,255,0) 50%;
+  --bottom_gradient: 0deg, var(--bg) -2%, rgba(255,255,255,0) 60%;
 
   width: 100%;
   height: 100%;
   z-index: 1;
   position: absolute;
-  opacity: .8;
 
   background: -moz-linear-gradient(var(--bottom_gradient));
   background: -webkit-linear-gradient(var(--bottom_gradient));
   background: linear-gradient(var(--bottom_gradient));
+  opacity: 1;
+
+  &::after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-color: rgba(0,0,0,.5);
+    background-image: url(${PLAY});
+    background-repeat: no-repeat;
+    background-position: center;
+    opacity: 0;
+    transition: all 200ms ease;
+  }
+
+  :hover::after {
+      opacity: 1;
+  }
 `;
 
 export const Number = styled.article`
