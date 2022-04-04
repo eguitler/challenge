151cca1654bf8a68c9dbbf486a0b6f7bcfd293de
Ezequiel Guitler <eguitler@gmail.com>
2022-04-03 23:23:50 -0300

    [feat] Home page added with Hero and Trailers

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Hero } from '../../components';
+import TrailerList from '../../components/TrailerList';
 
-const Home = () => (
-  <p style={{ color: 'white', fontSize: '4rem' }}>HOME</p>
-);
+const Home = () => {
+
+  const heroData = {
+    success: true,
+    data: {
+      _id: '614283e7fafdff084b66e527',
+      name: 'Blade Runner',
+      genre: 'Action',
+      duration: 190,
+      rating: 4.5,
+      views: 2500,
+      coverImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/c_scale,q_64,w_669/v1631745380/APICHALLANGE/blade_ndzgpn.png',
+      trailerImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/v1631747585/APICHALLANGE/trailer_s2u8kt.png',
+    },
+  };
+
+  const trailersData = {
+    success: true,
+    data: [
+      {
+        _id: '614285ec4fdb7108daae17bd',
+        trailerImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/v1631747585/APICHALLANGE/trailer5_o9bfjt.png',
+      },
+      {
+        _id: '6142861d4fdb7108daae17be',
+        trailerImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/v1631747585/APICHALLANGE/trailer2_w38qvq.png',
+      },
+      {
+        _id: '614286a44fdb7108daae17bf',
+        trailerImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/v1631747585/APICHALLANGE/trailer4_d7sy5h.png',
+      },
+      {
+        _id: '614286ee4fdb7108daae17c0',
+        trailerImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/v1631747585/APICHALLANGE/trailer3_euxvzy.png',
+      },
+      {
+        _id: '6142872b4fdb7108daae17c1',
+        trailerImage: 'https://res.cloudinary.com/dvmll0ruo/image/upload/v1631747585/APICHALLANGE/trailer_s2u8kt.png',
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Hero movie={heroData.data} />
+      <TrailerList trailers={trailersData.data} />
+    </>
+  );
+};
 
 export default Home;
