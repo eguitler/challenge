556455f5d0d731795f2cf4146233b739720a22b0
Ezequiel Guitler <eguitler@gmail.com>
2022-04-04 23:49:55 -0300

    [config] new assets: clock and eye

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 import STAR from './star.svg';
 import HALF_STAR from './half-star.svg';
 import PLAY from './play.svg';
+import CLOCK from './clock.svg';
+import EYE from './eye.svg';
 
 export {
   STAR,
   HALF_STAR,
   PLAY,
+  CLOCK,
+  EYE,
 };
