d74a4d5370f1e085109673c798bbd93cb5a23fbf
Ezequiel Guitler <eguitler@gmail.com>
2022-04-06 01:37:46 -0300

    [fix] fonts urls were incorrect

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
 
   * {
     margin: 0;
@@ -25,21 +25,19 @@ const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: 'DM Sans';
-    src: url('./fonts/DMSans-Bold.ttf');
+    src: url('/fonts/DMSans-Bold.ttf');
     font-weight: 700;
   }
 
   @font-face {
     font-family: 'DM Sans';
-    src: url('./fonts/DMSans-Medium.ttf');
+    src: url('/fonts/DMSans-Medium.ttf');
     font-weight: 500;
   }
 
   @font-face {
     font-family: 'DM Sans';
-    src: url('./fonts/DMSans-Regular.ttf');
+    src: url('/fonts/DMSans-Regular.ttf');
     font-weight: 400;
   }
 `;
-
-export default GlobalStyle;
