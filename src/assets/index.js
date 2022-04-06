e5a36f2504dbc078779bde669661151816e833f6
Ezequiel Guitler <eguitler@gmail.com>
2022-04-06 01:24:25 -0300

    [config] new assets added

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,14 @@ import HALF_STAR from './half-star.svg';
 import PLAY from './play.svg';
 import CLOCK from './clock.svg';
 import EYE from './eye.svg';
+import ARROW_RIGHT from './arrow-right.svg';
+import ARROW_LEFT from './arrow-left.svg';
+import LOCATION from './location.svg';
+import ENVELOP from './envelop.svg';
+import PHONE from './phone.svg';
+import SEARCH from './search.svg';
+import AVATAR from './avatar.svg';
+import NOT_FOUND from './404.png';
 
 export {
   STAR,
@@ -10,4 +18,12 @@ export {
   PLAY,
   CLOCK,
   EYE,
+  ARROW_RIGHT,
+  ARROW_LEFT,
+  LOCATION,
+  ENVELOP,
+  PHONE,
+  SEARCH,
+  AVATAR,
+  NOT_FOUND,
 };
