a4582fe4cbd9615eeca1ce9b6c3d2272382cc4a3
Ezequiel Guitler <eguitler@gmail.com>
2022-04-06 01:48:22 -0300

    [config] Footer, Header and Layout added to components index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-/* eslint-disable import/prefer-default-export */
 export { default as Hero } from './Hero';
 export { default as TrailerList } from './TrailerList';
 export { default as Featured } from './Featured';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Layout } from './Layout';
