31e8597dd8f7adaea1052ec8f94ccb325df028ac
Ezequiel Guitler <eguitler@gmail.com>
2022-04-06 01:47:10 -0300

    [feat] Movie page implemented

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Hero, TrailerList } from '../../components';
+import LoadingOverlay from '../../components/LoadingOverlay';
+import { useMovie } from '../../services';
 
-const Movie = () => (
-  <p style={{ color: 'white', fontSize: '4rem' }}>Movie</p>
-);
+const Movie = () => {
+
+  const { id } = useParams();
+
+  const {
+    data: movie,
+    isError: movieError,
+    isLoading: movieLoading,
+  } = useMovie(id);
+
+  if (movieLoading) return <LoadingOverlay />;
+
+  const trailer = [{
+    _id: movie._id,
+    trailerImage: movie.trailerImage,
+  }];
+
+  return (
+    <>
+      <Hero goBack='/' isError={movieError} movie={movie} />
+      <TrailerList isError={movieError} trailers={trailer} />
+    </>
+  );
+};
 
 export default Movie;
